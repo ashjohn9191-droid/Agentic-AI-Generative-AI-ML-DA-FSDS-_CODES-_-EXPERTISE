7ee107db2c0975711c3d2accfb7efa3262ee84fd
Ashley <ashjohn9191@gmail.com>
2026-01-23 01:09:37 +0530

    Add files via upload

---
+++ Xgboost.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset = pd.read_csv(r"C:\Users\ANITHA\Downloads\Churn_Modelling.csv")
+
+X = dataset.iloc[:,[3,-1]].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+X[:,1]= le.fit_transform(X[:,1])
+
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(
+    transformers=[('encoder', OneHotEncoder(), [1])],
+    remainder='passthrough'
+)
+
+X = np.array(ct.fit_transform(X))
+print(X)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+from xgboost import XGBClassifier
+classifier = XGBClassifier(random_state=0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+from sklearn.metrics import accuracy_score
+ac = accuracy_score(y_test, y_pred)
+print(ac)
+
+bias = classifier.score(X_train, y_train)
+bias
+
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimetor = classifier,X = X_train,y=y_train,cv=8)
+print("Accuracy: {:.2f) %".format(accurancies.mean()*100))
+
+
+
+
